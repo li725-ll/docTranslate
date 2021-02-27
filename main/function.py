5d70d1bfd82f97f212ac19e1aee7ece1d9aeea7b
笑笑 <8686805+li-mingxiao98@user.noreply.gitee.com>
2021-02-27 19:20:03 +0800

    update main/function.py.

--- main/function.py
+++ main/function.py
@@ -1,11 +1,16 @@
 import pygame,requests
+import json
 
-pygame.mixer.init()
-response = requests.get("https://staticedu-wps.cache.iciba.com/audio/fe7f398766ee10e40bbe65c1b44cf757.mp3")
-with open("music/1.mp3", "wb") as file:
-    file.write(response.content)
-pygame.mixer.music.load("music/1.mp3")
-pygame.mixer.music.play()
 
-while pygame.mixer.music.get_busy():
-    pass
+def music_play():
+    with open("json/1.json","r") as file:
+        text = json.loads(file.read())
+    print(text["tts"])
+    response = requests.get(text["tts"])
+    with open("music/OneSentenceADay/1.mp3", "wb") as file:  # 暂存文件
+        file.write(response.content)
+    pygame.mixer.init()
+    pygame.mixer.music.load("music/OneSentenceADay/1.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pass
