3516b0563cb255f12446b02202c04969ea9b1804
笑笑 <8686805+li-mingxiao98@user.noreply.gitee.com>
2021-02-27 19:20:48 +0800

    update main/sentence.py.

--- main/sentence.py
+++ main/sentence.py
@@ -1,13 +1,9 @@
 import requests
-import json
+import json,time
 
 header = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'
 }
-response = requests.get(url='http://open.iciba.com/dsapi/',params={'date':'2021-02-07'},headers=header)  # date参数为空代表今天,此处设置的为2021年2月7日
-response_next = requests.get(url='http://open.iciba.com/dsapi/',params={'date':'2021-02-07','type':'next'},headers=header)  # type参数为next表示今天的下一天
-response_last = requests.get(url='http://open.iciba.com/dsapi/',params={'date':'2021-02-07','type':'last'},headers=header)  # type参数为last表示今天的上一天
-print(response.json())  # 以json的格式接收返回的数据，因为file的默认格式为json
-print(response_next.json())
-print(response_last.json())
-
+response = requests.get(url='http://open.iciba.com/dsapi/',params={'date':time.strftime("%Y-%m-%d", time.localtime())},headers=header)  # date参数为空代表今天
+with open("json/1.json", "w", encoding="utf-8") as file:
+    file.write(json.dumps(response.json()))
